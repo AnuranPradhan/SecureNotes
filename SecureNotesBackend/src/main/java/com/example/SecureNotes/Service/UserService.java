96adfaf312a4dbbcb0f1b12969c5f4ff8d0c904d
Anuran Pradhan <anuranpradhan002@gmail.com>
2025-07-24 01:55:04 +0530

    Third Commit

---
+++ SecureNotesBackend/src/main/java/com/example/SecureNotes/Service/UserService.java
@@ -0,0 +1,66 @@
+package com.example.SecureNotes.Service;
+
+
+import com.example.SecureNotes.Repository.RoleRepository;
+import com.example.SecureNotes.Repository.UserRepository;
+import com.example.SecureNotes.dto.UserDTO;
+import com.example.SecureNotes.model.AppRole;
+import com.example.SecureNotes.model.Role;
+import com.example.SecureNotes.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+
+    public void updateUserRole(Long userId, String roleName) {
+        User user = userRepository.findById(userId).orElseThrow(()
+                -> new RuntimeException("User not found"));
+        AppRole appRole = AppRole.valueOf(roleName);
+        Role role = roleRepository.findByRoleName(appRole)
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+        user.setRole(role);
+        userRepository.save(user);
+    }
+
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+    public UserDTO getUserById(Long id) {
+//        return userRepository.findById(id).orElseThrow();
+        User user = userRepository.findById(id).orElseThrow();
+        return convertToDto(user);
+    }
+
+    private UserDTO convertToDto(User user) {
+        return new UserDTO(
+                user.getUserId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.isAccountNonLocked(),
+                user.isAccountNonExpired(),
+                user.isCredentialsNonExpired(),
+                user.isEnabled(),
+                user.getCredentialsExpiryDate(),
+                user.getAccountExpiryDate(),
+                user.getTwoFactorSecret(),
+                user.isTwoFactorEnabled(),
+                user.getSignUpMethod(),
+                user.getRole(),
+                user.getCreatedDate(),
+                user.getUpdatedDate()
+        );
+    }
+}
